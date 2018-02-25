21752e2a8f577ea6e654e32dc0462c708dd89c89
dinokeco <dino.keco@gmail.com>
2018-02-25 14:47:15 +0100

    unit test

--- src/ba/edu/ibu/ds/week1/BasicTest.java
+++ src/ba/edu/ibu/ds/week1/BasicTest.java
@@ -1,5 +1,35 @@
 package ba.edu.ibu.ds.week1;
 
-public class BasicTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BasicTest {
+	
+	private Basic basic;
+	
+	@Before
+	public void setup() {
+		this.basic = new Basic();     
+	}
+	
+	@Test
+	public void method1PositiveTest(){	
+		String msg = this.basic.method1();
+		assertEquals(msg, "This is method 1");
+	}
+
+	@Test
+	public void method1NegativeTest(){	
+		String msg = this.basic.method1();
+		assertNotEquals(msg, "This is method 2");
+	}
+	
+	@After
+	public void cleanup() {
+		this.basic = null;    
+	}
 }
