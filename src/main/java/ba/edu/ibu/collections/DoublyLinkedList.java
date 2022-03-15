f397b3234d55b5b53a743eb3ea4d5415be388e90
amarell <mujagicamar@gmail.com>
2022-03-15 17:49:07 +0100

    added DoublyLinkedList skeleton

---
+++ src/main/java/ba/edu/ibu/collections/DoublyLinkedList.java
@@ -0,0 +1,76 @@
+package ba.edu.ibu.collections;
+
+
+import java.util.Iterator;
+
+/**
+ * Doubly linked list implementation.
+ * <p>
+ * Implement the missing method code.
+ * Feel free to delete "return null", etc. lines; they're just placeholders.
+ *
+ * @author Aldin Kovačević
+ * @author Amar Mujagić
+ */
+public class DoublyLinkedList<Item> implements Iterable<Item> {
+    private Node2<Item> head;
+    private Node2<Item> tail;
+    private int size = 0;
+
+    /* Add a new node to the front of the doubly linked list */
+    public void addToFront(Item item) {
+        //your code
+    }
+
+    /* Remove a node from the front of the doubly linked list */
+    public void removeFromFront() {
+        //your code
+    }
+
+    /* Add a new node to the end of the doubly linked list */
+    public void addToRear(Item item) {
+        //your code
+    }
+
+    /* Remove a node at the end of the doubly linked list */
+    public void removeFromRear() {
+        //your code
+    }
+
+    /* Get a linked list node by index (0-indexed) */
+    public Item get(int index) {
+        //your code
+        return null;
+    }
+
+
+    /* Return the current size of the doubly linked list */
+    public int count() {
+        //your code
+        return 0;
+    }
+
+    /* Return an Iterator Object */
+    @Override
+    public Iterator<Item> iterator() {
+        //your code
+        return null;
+    }
+
+    /* Define the Iterator class, and hasNext() and next() methods */
+    public class DoublyLinkedListIterator implements Iterator<Item> {
+        //your code
+
+        @Override
+        public boolean hasNext() {
+            //your code
+            return false;
+        }
+
+        @Override
+        public Item next() {
+            //your code
+            return null;
+        }
+    }
+}
---
+++ src/main/java/ba/edu/ibu/collections/Node2.java
@@ -0,0 +1,7 @@
+package ba.edu.ibu.collections;
+
+public class Node2<Item> {
+    Item data;
+    Node2<Item> next;
+    Node2<Item> prev;
+}
