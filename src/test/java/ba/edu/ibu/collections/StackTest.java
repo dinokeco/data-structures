e168046493939e93070a37188183898a454e749e
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2022-03-15 15:33:07 +0100

    Minor updates.

--- src/test/java/ba/edu/ibu/collections/StackTest.java
+++ src/test/java/ba/edu/ibu/collections/StackTest.java
@@ -2,9 +2,7 @@ package ba.edu.ibu.collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
