03008ec5b621beaf7a6d7caf28141a1212cf7d55
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2022-03-23 09:00:41 +0100

    Updated test code for bubble sort.

---
+++ src/test/java/ba/edu/ibu/search/BubbleSortTest.java
@@ -0,0 +1,55 @@
+package ba.edu.ibu.search;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import ba.edu.ibu.sort.BubbleSort;
+
+@TestMethodOrder(OrderAnnotation.class)
+class BubbleSortTest {
+
+	@Test
+	@Order(1)
+	void testSortsARandomArray() {
+		int[] elements = {49, 16, 19, 17, 15, 53, 30, 90, 42, 11};
+		
+		BubbleSort.sort(elements);
+		
+		assertEquals(11, elements[0]);
+		assertEquals(19, elements[4]);
+		assertEquals(90, elements[9]);
+
+	}
+
+	@Test
+	@Order(2)
+	void testSortsASortedArray() {
+		int[] elements = {11, 15, 16, 17, 19, 30, 42, 49, 53, 90};
+		
+		BubbleSort.sort(elements);
+		
+		assertEquals(11, elements[0]);
+		assertEquals(19, elements[4]);
+		assertEquals(90, elements[9]);
+	}
+	
+	@Test
+	@Order(3)
+	void testSortsAReverslySortedArray() {
+		int[] elements = {90, 53, 49, 42, 30, 19, 17, 16, 15, 11};
+		
+		BubbleSort.sort(elements);
+		
+		assertEquals(11, elements[0]);
+		assertEquals(19, elements[4]);
+		assertEquals(90, elements[9]);
+	}
+}
